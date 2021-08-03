16ae031c7ae61766a6fd0fc43bd2377a447741b6
hopedorkenoo <compounddork@gmail.com>
2021-08-03 16:15:04 +0000

    env file

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(helmet());
 app.use(morgan("common"));
 
 app.use("/api/conversations", conversationsRoute);
-app.use("/api/auth",authroute)
+app.use("/api/auth", authroute)
 
 
 // MySql SetUp
