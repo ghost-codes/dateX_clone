140f002bb5bd95c15b385e05243336cbaf28f3da
hopedorkenoo <compounddork@gmail.com>
2021-09-10 15:57:05 +0000

    Authentication with jwt refresh token

--- router/auth.js
+++ router/auth.js
@@ -5,15 +5,22 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
-router.get('/users', (req, res) => { res.json(users) });
+router.get('/users', (req, res) => {
+    search = req.query.search
+    console.log(search)
+    var searchUser = `SELECT * FROM userAuth WHERE (username LIKE '%${search}%') AND isDeleted='0' `
+
+    db.query(searchUser, function (errQuery, resQuery) {
+        if (errQuery) {
+            res.send(errQuery)
+        } else {
+            res.send(resQuery[0])
+        }
+    })
+});
 router.post('/users', async (req, res) => {
     try {
-        //const salt = await bcrypt.genSalt()
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        //console.log(salt)
-        //console.log(hashedPassword)
-
-
     } catch {
         res.status(500).send()
     }
@@ -34,12 +41,15 @@ router.post('/signup', async (req, res) => {
         result => {
             const token = jwt.sign({ username: body.username, user_id: body.username }, process.env.JWT_KEY, { expiresIn: "1h" });
 
+            const refreshToken = jwt.sign({ username: body.username, user_id: body.username }, process.env.JWT_KEY, { expiresIn: "300d" });
+
             const sql = `INSERT INTO userAuth (user_id, name, email, password) VALUES ('${user_id}','${body.username}', '${body.email}', '${result}')`
             db.query(sql, (err, rows) => {
                 if (!err) {
                     res.status(200).json({
                         message: "Authentication Succesful",
-                        token: token
+                        accessToken: token,
+                        refreshToken: refreshToken,
                     });
                 } else {
                     console.log(err);
@@ -56,6 +66,42 @@ router.post('/signup', async (req, res) => {
     });
 });
 
+router.get('/refreshtoken', async (req, res) => {
+    try {
+        const refreshtoken = req.headers.authorization.split(" ")[1];
+
+        const decoded = jwt.verify(refreshtoken, process.env.JWT_KEY);
+        const sql = `SELECT * FROM userAuth WHERE name = '${decoded.username}'`
+        db.query(sql, (err, rows) => {
+            if (!err) {
+                if (rows.length >= 1) {
+
+                    const token = jwt.sign({ username: body.username, user_id: body.username }, process.env.JWT_KEY, { expiresIn: "1h" });
+
+
+                    res.status(200).json({
+                        message: "Authentication Succesful",
+                        user: {
+                            username: rows[0].username,
+                            user_id: rows[0].user_id,
+
+                        },
+                        token: token,
+                    });
+                }
+
+
+            } else {
+                console.log(err);
+                res.status(401).json({
+                    err: "Invalid Details"
+                });
+            }
+        })
+    } catch (err) {
+        return res.status(401).json({ message: 'Refresh Token Expired' })
+    }
+})
 
 
 router.post('/login', async (req, res) => {
@@ -64,7 +110,7 @@ router.post('/login', async (req, res) => {
     const user_id = uuidv4();
 
     const token = jwt.sign({ username: body.username, user_id: body.username }, process.env.JWT_KEY, { expiresIn: "1h" });
-    const sql = `SELECT * FROM userAuth WHERE name = '${body.username}' OR email = '${body.email}'`
+    const sql = `SELECT * FROM userAuth WHERE name = '${body.identity}' OR email = '${body.identity}'`
     db.query(sql, (err, rows) => {
         if (!err) {
             if (rows.length >= 1) {
